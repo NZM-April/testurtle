b006d00545874079cc028fb3315eec5ca503ea30
Nozomu Oki <itsuki2030@live.jp>
2018-12-04 19:35:03 +0900

    add file option

--- cmd/run.go
+++ cmd/run.go
@@ -1,18 +1,30 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/NZM-April/testurtle/turtle"
 	"github.com/spf13/cobra"
 )
 
+type Options struct {
+    config string
+}
+
+var (
+    o = &Options{}
+)
+
 func init() {
-    rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(runCmd)
+	runCmd.Flags().StringVarP(&o.config, "file", "f", "./testurtle.json", "config file")
 }
 
 var runCmd = &cobra.Command{
     Use:   "run",
     Short: "Start Testing.",
     Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("config file: %s", o.config)
 		turtle.Start()
 	},
 }
