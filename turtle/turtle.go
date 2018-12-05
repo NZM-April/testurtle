e94a7ac5a2a6e0b8a8d67c08bf011c5785fe11ee
Nozomu Oki <itsuki2030@live.jp>
2018-12-05 21:00:13 +0900

    implement status code

--- turtle/turtle.go
+++ turtle/turtle.go
@@ -15,6 +15,7 @@ type Check struct {
 	URL    string `json:"URL"`
 	Contain string `json:"contain"`
 	Title string `json:"title"`
+	Status int `json:"status"`
 }
 
 func Start(config string) {
@@ -77,6 +78,21 @@ func Patrol(checks []Check) {
 			title := FindTitle(newStr)
 			b := strings.Contains(title, c.Title)
 			if b == true {
+				fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
+				okNum++
+			} else {
+				fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
+				ngNum++
+			}
+		}
+
+		if c.Status != 0 {
+			fmt.Printf("[testurtle] %s : %d\n", c.URL, c.Status)
+			r, err := http.Get(c.URL)
+			if err != nil {
+				fmt.Printf("[testurtle] error! %s\n", err)
+			}
+			if r.StatusCode == c.Status {
 				fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
 				okNum++
 			} else {
