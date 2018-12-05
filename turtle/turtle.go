31792e5d200cd8095242f770500b7da952aafe7e
Nozomu Oki <itsuki2030@live.jp>
2018-12-05 20:25:26 +0900

    implement

--- turtle/turtle.go
+++ turtle/turtle.go
@@ -8,11 +8,13 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"regexp"
 )
 
 type Check struct {
 	URL    string `json:"URL"`
-	Target string `json:"target"`
+	Contain string `json:"contain"`
+	Title string `json:"title"`
 }
 
 func Start(config string) {
@@ -44,22 +46,55 @@ func Patrol(checks []Check) {
 	okNum := 0
 	ngNum := 0
 	for _, c := range checks {
-		fmt.Printf("[testurtle] %s : %s\n", c.URL, c.Target)
-		r, err := http.Get(c.URL)
-		if err != nil {
-			fmt.Printf("[testurtle] error! %s\n", err)
+		if c.Contain != "" {
+			fmt.Printf("[testurtle] %s : %s\n", c.URL, c.Contain)
+			r, err := http.Get(c.URL)
+			if err != nil {
+				fmt.Printf("[testurtle] error! %s\n", err)
+			}
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(r.Body)
+			newStr := buf.String()
+			b := strings.Contains(newStr, c.Contain)
+			if b == true {
+				fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
+				okNum++
+			} else {
+				fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
+				ngNum++
+			}
 		}
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(r.Body)
-		newStr := buf.String()
-		b := strings.Contains(newStr, c.Target)
-		if b == true {
-			fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
-			okNum++
-		} else {
-			fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
-			ngNum++
+
+		if c.Title != "" {
+			fmt.Printf("[testurtle] %s : %s\n", c.URL, c.Title)
+			r, err := http.Get(c.URL)
+			if err != nil {
+				fmt.Printf("[testurtle] error! %s\n", err)
+			}
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(r.Body)
+			newStr := buf.String()
+
+			// newStrから<title></title>にはさまれた文字を取り出す処理
+			title := FindTitle(newStr)
+
+			b := strings.Contains(title, c.Title)
+			if b == true {
+				fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
+				okNum++
+			} else {
+				fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
+				ngNum++
+			}
 		}
 		fmt.Printf("[testurtle] Test completed. OK: \x1b[32m%d\x1b[0m  NG: \x1b[31m%d\x1b[0m\n", okNum, ngNum)
 	}
+}
+
+func FindTitle(s string) string{
+	re := regexp.MustCompile("<title>" + `.*` + "</title>")
+	title := re.FindString(s)
+	a := strings.Replace(title, "<title>", "", 1)
+	trimed_Title := strings.Replace(a, "</title>", "", 1)
+	return trimed_Title
 }
