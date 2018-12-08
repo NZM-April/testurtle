f24a2354f67b9abd1cf555608fa3dce972f78d32
Nozomu Oki <itsuki2030@live.jp>
2018-12-08 17:20:11 +0900

    divide function

--- turtle/turtle.go
+++ turtle/turtle.go
@@ -22,6 +22,8 @@ type Items struct {
 	Status int `json:"status"`
 }
 
+var okNum, ngNum int
+
 func Start(config string) {
 	fmt.Println("[testurtle] Starts!🐢")
 	Turtling(config)
@@ -54,59 +56,18 @@ func JsonParse(config string) []Items{
 }
 
 func Patrol(items []Items) {
-	okNum := 0
-	ngNum := 0
 	for _, i := range items {
 		r, err := http.Get(i.URL)
+		if err != nil{
+			fmt.Println()
+		}
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(r.Body)
-		newStr := buf.String()
+		bodyStr := buf.String()
 
-		if i.Contain != "" {
-			var err error
-			fmt.Printf("[testurtle] %s : %s\n", i.URL, i.Contain)
-			if err != nil {
-				fmt.Printf("[testurtle] error! %s\n", err)
-			}
-			b := strings.Contains(newStr, i.Contain)
-			if b == true {
-				fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
-				okNum++
-			} else {
-				fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
-				ngNum++
-			}
-		}
-
-		if i.Title != "" {
-			fmt.Printf("[testurtle] %s : %s\n", i.URL, i.Title)
-			if err != nil {
-				fmt.Printf("[testurtle] error! %s\n", err)
-			}
-			title := FindTitle(newStr)
-			b := strings.Contains(title, i.Title)
-			if b == true {
-				fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
-				okNum++
-			} else {
-				fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
-				ngNum++
-			}
-		}
-
-		if i.Status != 0 {
-			fmt.Printf("[testurtle] %s : %d\n", i.URL, i.Status)
-			if err != nil {
-				fmt.Printf("[testurtle] error! %s\n", err)
-			}
-			if r.StatusCode == i.Status {
-				fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
-				okNum++
-			} else {
-				fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
-				ngNum++
-			}
-		}
+		ContainModule(bodyStr, i)
+		TitleModule(bodyStr, i)
+		StatusModule(r, i)
 	}
 	fmt.Printf("[testurtle] Test completed. OK: \x1b[32m%d\x1b[0m  NG: \x1b[31m%d\x1b[0m\n", okNum, ngNum)
 }
@@ -117,4 +78,58 @@ func FindTitle(s string) string{
 	a := strings.Replace(title, "<title>", "", 1)
 	trimed_Title := strings.Replace(a, "</title>", "", 1)
 	return trimed_Title
+}
+
+func ContainModule(bodyStr string, i Items){
+	if i.Contain != "" {
+		var err error
+		fmt.Printf("[testurtle] %s : %s\n", i.URL, i.Contain)
+		if err != nil {
+			fmt.Printf("[testurtle] error! %s\n", err)
+		}
+		b := strings.Contains(bodyStr, i.Contain)
+		if b == true {
+			fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
+			okNum++
+		} else {
+			fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
+			ngNum++
+		}
+	}
+}
+
+func TitleModule(bodyStr string, i Items){
+	if i.Title != "" {
+		var err error
+		fmt.Printf("[testurtle] %s : %s\n", i.URL, i.Title)
+		if err != nil {
+			fmt.Printf("[testurtle] error! %s\n", err)
+		}
+		title := FindTitle(bodyStr)
+		b := strings.Contains(title, i.Title)
+		if b == true {
+			fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
+			okNum++
+		} else {
+			fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
+			ngNum++
+		}
+	}
+}
+
+func StatusModule(r *http.Response, i Items){
+	if i.Status != 0 {
+		var err error
+		fmt.Printf("[testurtle] %s : %d\n", i.URL, i.Status)
+		if err != nil {
+			fmt.Printf("[testurtle] error! %s\n", err)
+		}
+		if r.StatusCode == i.Status {
+			fmt.Printf("%s \x1b[32m%s\x1b[0m\n", "[testurtle] =>", "ok")
+			okNum++
+		} else {
+			fmt.Printf("%s \x1b[31m%s\x1b[0m\n", "[testurtle] =>", "ng")
+			ngNum++
+		}
+	}
 }
