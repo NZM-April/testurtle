6f591a3f1b8164a38edd0c694e4ddd5f158064b2
Nozomu Oki <itsuki2030@live.jp>
2018-12-09 17:19:40 +0900

    add total

--- modules/title.go
+++ modules/title.go
@@ -4,6 +4,8 @@ import(
 	"regexp"
 	"strings"
 	"fmt"
+
+	"github.com/NZM-April/testurtle/total"
 )
 
 func FindTitle(s string) string {
@@ -23,7 +25,7 @@ func TitleModule(bodyStr string, i Items, okNum int, ngNum int)(int, int){
 		}
 		title := FindTitle(bodyStr)
 		b := strings.Contains(title, i.Title)
-		okNum, ngNum = Judgement(b, okNum, ngNum)
+		okNum, ngNum = total.Judgement(b, okNum, ngNum)
 	}
 	return okNum, ngNum
 }
--- total/total.go
+++ total/total.go
@@ -1,4 +1,4 @@
-package modules
+package total
 
 import "fmt"
 
